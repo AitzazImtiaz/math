be0008e5b01b4ff51c8cf06b8fd65de0a1941be5
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-01-05 09:40:22 -0800

    Update r.py

--- r.py
+++ r.py
@@ -1,40 +1,36 @@
+from ramanujan.enumerators.ParallelGCFEnumerator import ParallelGCFEnumerator
+from ramanujan.poly_domains.CartesianProductPolyDomain import CartesianProductPolyDomain
 from ramanujan.LHSHashTable import LHSHashTable
-from ramanujan.enumerators.EfficientGCFEnumerator import EfficientGCFEnumerator
-from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
 from ramanujan.constants import g_const_dict
 
 """
-This script enumerates GCFs for zeta(3).
-It uses a specific family of series that tends to generate conjectures for zeta(3)
-The family is defined under ramanujan.poly_domains.Zeta3Domain1
+This script enumerates GCFs for pi.
+
 It uses the following series:
-a(n) = (x0*n + x1)(x2*n*(n + 1) + x3)
-b(n) = x4*n^6
-We're setting x0 = 2 and x1 = 1.
-x2 and x3 are set to range from -50 to 50
-x4 is set to range from -10 to -1
-Also, Zeta3Domain1 will check for a convergence condition for the GCF, as described in http://www.ramanujanmachine.com/
+an = x0 * n + x1
+bn = x0 * n^2 + x1 * n + x3
+When an coefs can range between -13 and 13, bn coefs can range from -11 to 11.
+All coefs are independent from one another and all combinations will be generated.
 """
 
-# create a LHS table for zeta
-saved_hash = 'zeta3.lhs.dept20.db'
+# create a LHS table for pi
+saved_hash = 'pi.lhs.dept20.db'
 lhs_search_limit = 20
 lhs = LHSHashTable(
     saved_hash,
     lhs_search_limit,
-    [g_const_dict['zeta'](4)])
+    [g_const_dict['pi']]) 
 
 # define the poly domain
-poly_search_domain = Zeta3Domain1(
-    [(2, 2), (1, 1), (-50, 50), (-50, 50)],
-    (-10, -1))
+poly_search_domain = CartesianProductPolyDomain(
+    1, [15, 35],
+    2, [-11, 12])
 
-# create an enumerator to iter thought the poly domain and compare it to the lhs table
-enumerator = EfficientGCFEnumerator(
+# create an enumerator that iters thought the poly domain and compare GCFs to the lhs table
+enumerator = ParallelGCFEnumerator(
     lhs,
     poly_search_domain,
-    [g_const_dict['zeta'](4)]
-)
+    [g_const_dict['pi']])
 
 results = enumerator.full_execution()
 print("{} results found!".format(len(results)))
